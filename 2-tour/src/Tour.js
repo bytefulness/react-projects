39cc1bcf745e85ce36bd9f474a489449503cf6a1
eakyel <e.akyel@outlook.com>
2021-04-08 09:26:22 +0300

    CREATE: (Tour) - Create project structure

---
+++ 2-tour/src/Tour.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const Tour = () => {
+  return;
+};
+
+export default Tour;
---
+++ 2-tour/src/Tours.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours }) => {
+  return;
+};
+
+export default Tours;
