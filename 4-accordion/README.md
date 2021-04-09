716cca7c631574c00e3b62ee32b053e16307231e
eakyel <e.akyel@outlook.com>
2021-04-09 09:04:39 +0300

    DONE: (Accordion) - Edit styles for UX

---
+++ 4-accordion/README.md
@@ -0,0 +1,22 @@
+# Accordion
+
+#### Idea
+
+[uidesigndaily](https://uidesigndaily.com/posts/sketch-accordion-website-day-1175)
+
+![](./idea.png);
+
+#### React Icons
+
+[react icons](https://react-icons.github.io/react-icons/)
+
+```
+npm/yarn install react-icons
+```
+
+```javascript
+import { FaHome } from "react-icons/fa";
+const Component = () => {
+  return <FaHome className="icon"></FaHome>;
+};
+```
---
+++ 4-accordion/src/Question.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  return (
+    <article className="question" onClick={() => setShowInfo(!showInfo)}>
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
+
+export default Question;
