7934c62d05f4262c0e08052c9dbbd555dda2f5e8
eakyel <e.akyel@outlook.com>
2021-04-09 18:18:33 +0300

    DONE: (food-menu) - Dislay categories and items dynamically

--- 5-food-menu/src/App.js
+++ 5-food-menu/src/App.js
@@ -3,17 +3,30 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item) => category === item.category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
       <section className="menu section">
-        <h2>Our Menu</h2>
-        <div className="underline"></div>
-
-        <Categories />
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
