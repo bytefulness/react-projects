941bd8ed5db2687b0739a190121b50edcddfa31d
eakyel <e.akyel@outlook.com>
2021-04-11 07:13:26 +0300

    PROGRESS: (tabs) - Fetch jobs data from API

--- 6-tabs/src/App.js
+++ 6-tabs/src/App.js
@@ -1,10 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const jobs = await response.json();
+    setJobs(jobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  return <h2>tabs project setup</h2>;
 }
 
-export default App
+export default App;
