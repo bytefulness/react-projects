a33622d25c87a8595d52946e87cc7d9a3d2f7beb
eakyel <e.akyel@outlook.com>
2021-04-11 07:31:23 +0300

    PROGRESS: (tabs) - Display job at UI

--- 6-tabs/src/App.js
+++ 6-tabs/src/App.js
@@ -27,7 +27,33 @@ function App() {
     );
   }
 
-  return <h2>tabs project setup</h2>;
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App;
