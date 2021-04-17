d9feaa8f84d912517cc2824c18c0367935681143
eakyel <e.akyel@outlook.com>
2021-04-17 08:59:09 +0300

    PROGRESS: (lorem-ipsum) - Create UI component

--- 8-lorem-ipsum/src/App.js
+++ 8-lorem-ipsum/src/App.js
@@ -1,11 +1,33 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section section-center">
+      <h3>Tired of Boring Lorem Ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs: </label>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          min="0"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default App;
