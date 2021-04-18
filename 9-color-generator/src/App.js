3da8b89d0bd478da11f0a953ee1f49f547424458
eakyel <e.akyel@outlook.com>
2021-04-18 07:40:47 +0300

    PROGRESS: (color-generator) - Create component structure

--- 9-color-generator/src/App.js
+++ 9-color-generator/src/App.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Hello, world");
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section>
+        <h4>List goes here</h4>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
