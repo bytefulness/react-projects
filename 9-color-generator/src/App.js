aead7228fc34c4d448224fa5d6c7a0eb845efddd
eakyel <e.akyel@outlook.com>
2021-04-18 08:57:29 +0300

    PROGRESS: (color-generator) - Generate the color versions

--- 9-color-generator/src/App.js
+++ 9-color-generator/src/App.js
@@ -12,6 +12,7 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
+      setList(colors);
     } catch (error) {
       setError(true);
       console.log(error);
@@ -35,8 +36,17 @@ function App() {
           </button>
         </form>
       </section>
-      <section>
-        <h4>List goes here</h4>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
--- 9-color-generator/src/SingleColor.js
+++ 9-color-generator/src/SingleColor.js
@@ -1,8 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  // const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
 
-export default SingleColor
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+    </article>
+  );
+};
+
+export default SingleColor;
