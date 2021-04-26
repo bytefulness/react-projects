4a8c3a5be7070010bfd794f2a5260bdfea675b3a
eakyel <e.akyel@outlook.com>
2021-04-26 17:33:15 +0300

    PROGRESS: (navbar) - Create navbar structure

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -1,10 +1,44 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTicketAlt, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+          </ul>
+        </div>
+        <ul className="social-icons">
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
