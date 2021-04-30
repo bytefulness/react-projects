d495dd726679f8b49c59b60263c6d14812a34649
eakyel <e.akyel@outlook.com>
2021-04-30 17:24:59 +0300

    PROGRESS: (stripe) - Create navbar structure

--- 13-stripe/src/Navbar.js
+++ 13-stripe/src/Navbar.js
@@ -1,9 +1,35 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 
-export default Navbar
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="stirpe" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">Products</button>
+          </li>
+          <li>
+            <button className="link-btn">Developers</button>
+          </li>
+          <li>
+            <button className="link-btn">Company</button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">Sign In</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
