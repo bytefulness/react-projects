04d5ca757d28b60bc829869554235e6a10b1b6b9
eakyel <e.akyel@outlook.com>
2021-04-30 17:38:35 +0300

    PROGRESS: (stripe) - Create hero component structure

--- 13-stripe/src/Hero.js
+++ 13-stripe/src/Hero.js
@@ -3,10 +3,28 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
+  const { closeSubmenu } = useGlobalContext();
 
-  return <h2>hero component</h2>;
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+
+          <button className="btn">Start Now</button>
+        </article>
+
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
