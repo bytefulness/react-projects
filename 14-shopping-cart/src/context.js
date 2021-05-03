7b8c1cf27d39f3de5b44b7ea56132a0c05336796
eakyel <e.akyel@outlook.com>
2021-05-03 18:12:16 +0300

    CREATE: (shopping-cart) - Create project structure

---
+++ 14-shopping-cart/src/context.js
@@ -0,0 +1,34 @@
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-useReducer-cart-project";
+const AppContext = React.createContext();
+
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
