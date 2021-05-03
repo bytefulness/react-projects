fff72d7f6a4be8a7f5c83855b9c42fec88b6617c
eakyel <e.akyel@outlook.com>
2021-05-03 15:22:54 +0300

    DONE: (stripe) - Style more for well UI

--- 13-stripe/src/Submenu.js
+++ 13-stripe/src/Submenu.js
@@ -9,10 +9,18 @@ const Submenu = () => {
   } = useGlobalContext();
 
   const [columns, setColumns] = useState("col-2");
+  const [isDescriptionExist, setIsDescriptionExist] = useState(false);
   const container = useRef(null);
 
   useEffect(() => {
     setColumns("col-2");
+
+    const value = links.every((link) => link.description);
+
+    console.log(links);
+    console.log(value);
+    setIsDescriptionExist(value);
+
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
@@ -33,7 +41,7 @@ const Submenu = () => {
       ref={container}
     >
       <h4>{page}</h4>
-      <div className={`submenu-center ${columns}`}>
+      {/* <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
           const { label, url, icon } = link;
           return (
@@ -43,7 +51,36 @@ const Submenu = () => {
             </a>
           );
         })}
-      </div>
+      </div> */}
+
+      {isDescriptionExist ? (
+        <div className="great">
+          {links.map((link, index) => {
+            const { label, url, icon, description } = link;
+            return (
+              <a href={url} className="link-item">
+                <span className="icon-container">{icon}</span>
+                <span className="title-container">
+                  <span className="title">{label}</span>
+                  <span className="description">{description}</span>
+                </span>
+              </a>
+            );
+          })}
+        </div>
+      ) : (
+        <div className={`submenu-center ${columns}`}>
+          {links.map((link, index) => {
+            const { label, url, icon } = link;
+            return (
+              <a key={index} href={url}>
+                {icon}
+                {label}
+              </a>
+            );
+          })}
+        </div>
+      )}
     </aside>
   );
 };
--- 13-stripe/src/data.js
+++ 13-stripe/src/data.js
@@ -1,28 +1,43 @@
-import { FaCreditCard, FaBook, FaBriefcase } from 'react-icons/fa';
-import React from 'react';
+import { FaCreditCard, FaBook, FaBriefcase } from "react-icons/fa";
+import React from "react";
 const sublinks = [
   {
-    page: 'products',
+    page: "products",
     links: [
-      { label: 'payment', icon: <FaCreditCard />, url: '/products' },
-      { label: 'terminal', icon: <FaCreditCard />, url: '/products' },
-      { label: 'connect', icon: <FaCreditCard />, url: '/products' },
+      {
+        label: "payment",
+        icon: <FaCreditCard />,
+        url: "/products",
+        description: "Online payments",
+      },
+      {
+        label: "terminal",
+        icon: <FaCreditCard />,
+        url: "/products",
+        description: "In-person payments",
+      },
+      {
+        label: "connect",
+        icon: <FaCreditCard />,
+        url: "/products",
+        description: "Payments for platforms",
+      },
     ],
   },
   {
-    page: 'developers',
+    page: "developers",
     links: [
-      { label: 'plugins', icon: <FaBook />, url: '/products' },
-      { label: 'libraries', icon: <FaBook />, url: '/products' },
-      { label: 'help', icon: <FaBook />, url: '/products' },
-      { label: 'billing', icon: <FaBook />, url: '/products' },
+      { label: "plugins", icon: <FaBook />, url: "/products" },
+      { label: "libraries", icon: <FaBook />, url: "/products" },
+      { label: "help", icon: <FaBook />, url: "/products" },
+      { label: "billing", icon: <FaBook />, url: "/products" },
     ],
   },
   {
-    page: 'company',
+    page: "company",
     links: [
-      { label: 'about', icon: <FaBriefcase />, url: '/products' },
-      { label: 'customers', icon: <FaBriefcase />, url: '/products' },
+      { label: "about", icon: <FaBriefcase />, url: "/products" },
+      { label: "customers", icon: <FaBriefcase />, url: "/products" },
     ],
   },
 ];
