78016a64632690cc53b9d7915f1998f18696c2f0
eakyel <e.akyel@outlook.com>
2021-05-06 19:07:13 +0300

    PROGRESS: (cocktails) - Create Navbar component

--- 15-cocktails/src/components/Navbar.js
+++ 15-cocktails/src/components/Navbar.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../logo.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
-  )
-}
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="cocktail" className="logo" />
+        </Link>
 
-export default Navbar
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
