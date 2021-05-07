716a169c4f88f9a02f362e22be413ba59907da05
eakyel <e.akyel@outlook.com>
2021-05-07 16:32:24 +0300

    PROGRESS: (cocktails) - Display cocktails list

--- 15-cocktails/src/components/CocktailList.js
+++ 15-cocktails/src/components/CocktailList.js
@@ -19,9 +19,15 @@ const CocktailList = () => {
   }
 
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className="section">
+      <h2 className="section-title">Cocktails</h2>
+
+      <div className="cocktails-center">
+        {cocktails.map((cocktail) => {
+          return <Cocktail key={cocktail.id} {...cocktail} />;
+        })}
+      </div>
+    </section>
   );
 };
 
