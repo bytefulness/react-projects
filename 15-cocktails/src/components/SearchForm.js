6c981814aa4d21193110c7ec1e32e60fc1a79ae1
eakyel <e.akyel@outlook.com>
2021-05-07 16:48:55 +0300

    PROGRESS: (cocktails) - Setup search form

--- 15-cocktails/src/components/SearchForm.js
+++ 15-cocktails/src/components/SearchForm.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef(null);
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    // Get input value
+    const value = searchValue.current.value;
+    // Run setSearchTerm to run useEffect
+
+    setSearchTerm(value);
+  };
 
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+        <div className="form-control">
+          <label htmlFor="name">Search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
