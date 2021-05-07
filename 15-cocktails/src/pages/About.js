767242b2f8b58fd1174e66653ccf5fbc1c0eacf7
eakyel <e.akyel@outlook.com>
2021-05-07 15:09:20 +0300

    PROGRESS: (cocktails) - Create error and about pages

--- 15-cocktails/src/pages/About.js
+++ 15-cocktails/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quo
+        molestiae numquam quas fugit soluta. Ratione veniam debitis officia, hic
+        consequatur corrupti sunt qui ut, nobis sapiente laudantium.
+        Perspiciatis possimus recusandae voluptatum corporis officiis sed
+        adipisci assumenda aliquid esse autem?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
