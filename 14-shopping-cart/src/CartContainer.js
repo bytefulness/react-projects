726d243f63eabb9138f9edc51a856d5f42ffe647
eakyel <e.akyel@outlook.com>
2021-05-09 19:25:58 +0300

    PROGRESS: (shopping-cart) - Redesign cart total UI

--- 14-shopping-cart/src/CartContainer.js
+++ 14-shopping-cart/src/CartContainer.js
@@ -1,4 +1,7 @@
-import React from "react";
+import { ReactComponent as Apple } from "./images/payment-apple.svg";
+import { ReactComponent as Visa } from "./images/payment-visa.svg";
+import { ReactComponent as Paypal } from "./images/payment-paypal.svg";
+import React, { useEffect } from "react";
 import CartItem from "./CartItem";
 import { useGlobalContext } from "./context";
 
@@ -16,8 +19,7 @@ const CartContainer = () => {
     );
   }
   return (
-    <section className="cart">
-      {/* cart header */}
+    <section className="checkout">
       <header className="cart__header">
         <h2 className="cart__title">shopping cart</h2>
         <span className="cart__explanation">
@@ -25,24 +27,44 @@ const CartContainer = () => {
           your cart
         </span>
       </header>
-      {/* cart items */}
-      <div>
-        {cart.map((item) => {
-          return <CartItem key={item.id} {...item} />;
-        })}
-      </div>
-      {/* cart footer */}
-      <footer>
-        <hr />
-        <div className="cart-total">
-          <h4>
-            total <span>${total}</span>
-          </h4>
+
+      <div className="cart-wrapper">
+        <div className="cart">
+          {/* cart items */}
+          <div>
+            {cart.map((item) => {
+              return <CartItem key={item.id} {...item} />;
+            })}
+          </div>
         </div>
-        <button className="btn clear-btn" onClick={clearCart}>
-          clear cart
-        </button>
-      </footer>
+
+        <div className="cart-total">
+          <p className="total__header">
+            Total <span className="small">(incl VAT)</span>
+          </p>
+          <p className="total__price">${total}</p>
+          <hr className="total__seperator" />
+
+          <div className="payment">
+            <h4 className="payment__header">payment methods</h4>
+            <div className="payment__group">
+              <button className="payment__button">
+                <Visa />
+              </button>
+              <button className="payment__button">
+                <Paypal />
+              </button>
+              <button className="payment__button">
+                <Apple />
+              </button>
+            </div>
+            <button className="pay">Pay</button>
+            <button className="remove" onClick={clearCart}>
+              Clear Cart
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
